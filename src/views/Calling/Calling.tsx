9c36d977335879969a3d1d09f32c34efee289b66
Joosung Ahn <joosung21@gmail.com>
2024-11-23 09:22:36 -0500

    Fix UI

--- src/views/Calling/Calling.tsx
+++ src/views/Calling/Calling.tsx
@@ -204,7 +204,7 @@ export default function Calling() {
               )}
 
               {/* Dummy div for auto-scroll */}
-              <div ref={conversationEndRef} className="mt-10" />
+              <div ref={conversationEndRef} className="mt-11" />
             </Stack>
           </div>
         </div>
