6819cec56e1744c3b6a4eb303563e7625431c4d6
Joosung Ahn <joosung21@gmail.com>
2024-11-23 01:07:57 -0500

    Try submit btn

--- src/app/Auth/Register/RegisterInformation.tsx
+++ src/app/Auth/Register/RegisterInformation.tsx
@@ -115,13 +115,14 @@ export default function RegisterInformation() {
                 />
               )}
             />
+            <button type="submit" style={{ display: 'none' }} />
           </Stack>
+          <div className="bottom-action">
+            <Button size="lg" onClick={handleSubmit(onSubmit)}>
+              Next
+            </Button>
+          </div>
         </form>
-        <div className="bottom-action">
-          <Button size="lg" onClick={handleSubmit(onSubmit)}>
-            Next
-          </Button>
-        </div>
       </div>
     </>
   );
